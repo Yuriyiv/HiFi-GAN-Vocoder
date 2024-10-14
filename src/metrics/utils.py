2bec068bab58d6ef506f78ecd193ed542b16cca3
Yuriyiv <yura.ivanov.2001@mail.ru>
2024-10-14 17:04:47 +0300

    Implement calculation of WER and CER metrics

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,19 @@
 # Based on seminar materials
+import editdistance
 
 # Don't forget to support cases when target_text == ''
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    if len(target_text) == 0:
+        return 1
+    target_text_splitted = target_text.split()
+    return editdistance.eval(target_text_splitted, predicted_text.split()) / len(
+        target_text_splitted
+    )
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    if len(target_text) == 0:
+        return 1
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
