d56dc2c78a87ad81705ba9f1e859b7928be5d18c
Yuriyiv <yura.ivanov.2001@mail.ru>
2024-10-16 19:22:33 +0300

    Fix a bug with string format specifier

--- src/trainer/base_trainer.py
+++ src/trainer/base_trainer.py
@@ -178,8 +178,7 @@ class BaseTrainer:
 
             # print logged information to the screen
             for key, value in logs.items():
-                self.logger.info(f"    {key: 15s}: {value}")
-
+                self.logger.info(f"{key:15s}: {value}")  # noqa: E231
             # evaluate model performance according to configured metric,
             # save best checkpoint as model_best
             best, stop_process, not_improved_count = self._monitor_performance(
